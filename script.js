4640cafefc74a79d8da597edb67154da4b8803cd
gemaquejr <gemaquejr@hotmail.com>
2023-03-06 11:12:07 -0300

    Adiciona codigo refatorado

--- script.js
+++ script.js
@@ -1,39 +1,86 @@
-const theButton = document.querySelector('.theButton');
-const tasklist = document.querySelector('.lista-tarefas');
-const text = document.querySelector('.insert');
+const taskText = document.querySelector('#texto-tarefa');
+const taskList = document.querySelector('#lista-tarefas');
+const body = document.querySelector('body');
+const createTask = document.querySelector('#criar-tarefa');
+const clearAll = document.querySelector('#apaga-tudo');
+const clearCompleted = document.querySelector('#remover-finalizados');
+const removedSelected = document.querySelector('#remover-selecionado');
+const upwards = document.querySelector('#mover-cima');
+const below = document.querySelector('#mover-baixo');
+const saveTask = document.querySelector('#salvar-tarefas');
 
-function formulary() {
-  const list = document.createElement('li');
-  list.innerHTML = text.value;
-  list.classList.add('selected');
-  tasklist.appendChild(list);
-  text.value = '';
-}
+createTask.addEventListener('click', () => {
+  const newTask = document.createElement('li');
+  newTask.className = 'task';
+  newTask.innerHTML = taskText.value;
+  taskList.appendChild(newTask);
+  taskText.value = '';
+});
 
-theButton.addEventListener('click', formulary);
-
-function bgList(event) {
-  const e = event.target;
-  e.style.backgroundColor = 'rgb(128, 128, 128)';
-}
-tasklist.addEventListener('click', bgList);
-
-function selectedTask(event) {
-  const eventTargetSelect = event.target;
-  const checkStyle = document.querySelectorAll('li');
-  for (let i = 0; i < checkStyle.length; i += 1) {
-    checkStyle[i].style.backgroundColor = '';
+function changeBackgroundColor(e) {
+  for (let index = 0; index < taskList.children.length; index += 1) {
+    if (taskList.children[index].classList.contains('selected')) {
+      taskList.children[index].classList.remove('selected');
+    }
   }
-  eventTargetSelect.style.backgroundColor = 'rgb(128, 128, 128)';
+  const task = e.target;
+  task.classList.add('selected');
 }
-tasklist.addEventListener('click', selectedTask);
 
-const eraserButton = document.getElementById('apaga-tudo');
-
-eraserButton.addEventListener('click', () => {
-  const list = document.querySelectorAll('li');
-
-  for (let i = 0; i < list.length; i += 1) {
-    document.querySelector('#lista-tarefas').removeChild(list[i]);
+body.addEventListener('click', (e) => {
+  if (e.target.classList.contains('task')) {
+    changeBackgroundColor(e);
   }
 });
+
+taskList.addEventListener('dblclick', (event) => {
+  event.target.classList.toggle('completed');
+});
+
+clearAll.addEventListener('click', () => {
+  taskList.innerHTML = '';
+});
+
+function removeCompletedTask() {
+  const completedTasks = document.querySelectorAll('.completed');
+  for (let index = 0; index < completedTasks.length; index += 1) {
+    taskList.removeChild(completedTasks[index]);
+  }
+}
+clearCompleted.addEventListener('click', removeCompletedTask);
+
+function saveTaskStorage() {
+  localStorage.setItem('taskListStorage', JSON.stringify(taskList.innerHTML));
+}
+saveTask.addEventListener('click', saveTaskStorage);
+
+window.onload = () => {
+  if (localStorage !== null) {
+    const returnTaskHTML = JSON.parse(localStorage.getItem('taskListStorage'));
+    taskList.innerHTML = returnTaskHTML;
+  }
+};
+
+function moveUp() {
+  const selectedTask = document.querySelector('.selected');
+  if (selectedTask !== null && selectedTask.previousElementSibling) {
+    const itemBefore = selectedTask.previousElementSibling;
+    taskList.insertBefore(selectedTask, itemBefore);
+  }
+}
+upwards.addEventListener('click', moveUp);
+
+function moveDown() {
+  const selectedTask = document.querySelector('.selected');
+  if (selectedTask !== null && selectedTask.nextElementSibling) {
+    const itemAfter = selectedTask.nextElementSibling;
+    itemAfter.after(selectedTask);
+  }
+}
+below.addEventListener('click', moveDown);
+
+function removeSelectedTask() {
+  const selectedTask = document.querySelector('.selected');
+  taskList.removeChild(selectedTask);
+}
+removedSelected.addEventListener('click', removeSelectedTask);
